249a268ce2dfe39ef3daa62f784642860d5f4df0
Hitesh Gulati <hg0111@gmail.com>
2022-06-21 16:20:03 +0530

    position ans instrument completed

---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def to_currency (value, multiplier=100, add_currency_symbol=True, currency_symbol = '₹',rounding=0):
+    negative_sign = ""
+    if value < 0:
+        negative_sign = "-"
+        value = abs(value)
+    value = round(value,rounding)
+    decimal_value = round(value%1,rounding)
+    value = int(round((value-decimal_value),0))
+    if decimal_value == 0:
+        currency_string = "" 
+    else:
+        currency_string = "." + str(decimal_value)[2:]
+    base = 1_000
+    set = value%base
+    currency_string = str(int(round(set,0))) + currency_string
+    if (len(str(set)) < len(str(value))) & (len(str(set)) < len(str(base))-1):
+        zeros = "".join(['0' for _ in range(len(str(base))-1-len(str(set)))])
+        currency_string = zeros + currency_string
+        
+    converted_value = set
+    if set == value:
+        if add_currency_symbol:
+            return currency_symbol + " " + negative_sign + currency_string
+        else: 
+            return negative_sign + currency_string
+    else:
+        while converted_value != value:
+            base = base * multiplier
+            set = int(round((value%base - converted_value)/base * multiplier,0))
+            currency_string = str(int(set)) + "," + currency_string
+            if (len(str(int(round(set*base/multiplier,0)))) < len(str(value))) & (len(str(set)) < len(str(multiplier))-1):
+                zeros = "".join(['0' for _ in range(len(str(multiplier))-1-len(str(set)))])
+                currency_string = zeros + currency_string
+            converted_value = int(round(converted_value + (set*base/multiplier),0))
+
+    if add_currency_symbol:
+        return currency_symbol + " " + negative_sign + currency_string
+    else: 
+        return negative_sign + currency_string
+
+print(to_currency(-78361.78264643789, multiplier=100,rounding=0, add_currency_symbol=True))
